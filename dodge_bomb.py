3fcce2b35b6cc2cc50f5144a12df7f3cb6d86f79
c0a24219da <c0a24219da@edu.teu.ac.jp>
2025-09-30 14:43:34 +0900

    練習4:ゲームオーバーの判定

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -48,7 +48,9 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
-        screen.blit(bg_img, [0, 0]) 
+        screen.blit(bg_img, [0, 0])
+        if kk_rct.colliderect(bb_rct):  # こうかとんと爆弾の衝突判定
+            return  # ゲームオーバー
 
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
